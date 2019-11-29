5c75df189a9fb21f6416c44921fcaaf7aeaf6d35
迪南 林 <eamdfan@hotmail.com>
2019-11-29 20:33:30 +0800

    添加项目文件。

---
+++ CMPSAPP/CMPSAPP/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CMPSAPP.Models
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
